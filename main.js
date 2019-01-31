d7b40954bc4dedeb2f9e21020dd7c2851e8a4181
LeonnardoVerol <6124410+LeonnardoVerol@users.noreply.github.com>
2019-01-31 08:32:46 -0200

    :tada: Firt Commit

---
+++ main.js
@@ -0,0 +1,21 @@
+
+
+function operacaoMatematica (codigo)
+{
+    if(codigo == 1)
+    {
+        //
+    }
+    else if(codigo == 2)
+    {
+        //
+    }
+    else if(codigo == 3)
+    {
+        //
+    }
+    else if(codigo == 4)
+    {
+        //
+    }
+}
