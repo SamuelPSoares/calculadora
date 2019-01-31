562024007adddcfd1a155a83f5ef44925085e053
LeonnardoVerol <6124410+LeonnardoVerol@users.noreply.github.com>
2019-01-31 08:44:56 -0200

    Adiciona Divisao

--- main.js
+++ main.js
@@ -2,6 +2,9 @@
 
 function operacaoMatematica (codigo)
 {
+    var num1 = document.getElementById("primeiroNumero").value;
+    var num2 = document.getElementById("segundoNumero").value;
+
     if(codigo == 1)
     {
         //
@@ -11,8 +14,15 @@ function operacaoMatematica (codigo)
         //
     }
     else if(codigo == 3)
-    {
-        //
+    {   
+        if(num2 == 0)
+        {
+            alert("Tira esse Zero dae");
+        }
+        
+        var resp = num1/num2;
+        document.getElementById("resultado").value = resp;
+
     }
     else if(codigo == 4)
     {
