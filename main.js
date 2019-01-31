74d5943195bf4618de35ba000f8c9787c702a50c
Calebe Santos <calebe.bsantos@outlook.com>
2019-01-31 08:42:52 -0200

    add multiply

--- main.js
+++ main.js
@@ -2,6 +2,18 @@
 
 function operacaoMatematica (codigo)
 {
+    var num1 = document.getElementById('primeiroNumero').value
+    var num2 = document.getElementById('segundoNumero').value
+
+    var resultadoElement = document.getElementById('resultado')
+
+    if (isNaN(num1) && isNaN(num2)) {
+        resultadoElement.value = 'Erro'
+        return
+    }
+
+    var resultado = ''
+
     if(codigo == 1)
     {
         //
@@ -16,6 +28,8 @@ function operacaoMatematica (codigo)
     }
     else if(codigo == 4)
     {
-        //
+        resultado = num1 * num2
     }
+
+    resultadoElement.value = resultado
 }
